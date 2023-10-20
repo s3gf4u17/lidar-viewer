5fd4efb4771f8259ecc26ddfaac595cc1d022e9e
s3gf4u17 <szymon.kolakowski@gmail.com>
2023-10-20 04:10:03 +0200

    view reset ui

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,16 @@
 #include <iostream>
 #include "lib.h"
 
-int SCREEN_WIDTH = 1400;
-int SCREEN_HEIGHT = 1000;
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 
-float rotation = 0.0;
-float scale = 0.1;
+#define SCREEN_WIDTH 1500
+#define SCREEN_HEIGHT 1000
+
+float rotationx = 0.0;
+float rotationy = 0.0;
+float scale = 0.6;
 bool rotating = false;
 bool moving = false;
 
@@ -21,12 +26,10 @@ double lastx,lasty;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
-    SCREEN_WIDTH = width;
-    SCREEN_HEIGHT = height;
 }
 
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
-    scale = (yoffset>0) ? scale*1.5 : (yoffset<0) ? scale/1.5 : scale;
+    scale = (yoffset>0) ? scale*1.2 : (yoffset<0) ? scale/1.5 : scale;
 }
 
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
@@ -49,7 +52,8 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 
 static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos) {
     if (rotating) {
-        rotation = rotation + (lastx-xpos)/2;
+        rotationx = rotationx + (lastx-xpos)/2;
+        // rotationy = rotationy + (ypos-lasty)/2;
     }
     if (moving) {
         movex = movex + std::max(std::min((xpos-lastx)/scale,1.0),-1.0);
@@ -58,7 +62,6 @@ static void cursor_position_callback(GLFWwindow* window, double xpos, double ypo
     lastx = xpos;
     lasty = ypos;
 }
-
 
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
@@ -93,6 +96,16 @@ int main() {
     glfwSetMouseButtonCallback(window, mouse_button_callback);
     glfwSetCursorPosCallback(window, cursor_position_callback);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // imgui setup
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();(void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330 core");
 
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
@@ -131,7 +144,7 @@ int main() {
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    LasFile lasfile("../data/lidar_rysy.las");
+    LasFile lasfile("../data/lidar_stadion.las");
     std::cout << lasfile << std::endl;
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
@@ -146,44 +159,56 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
     glEnable(GL_PROGRAM_POINT_SIZE);
-    glPointSize(1);
+    glPointSize(2);
     glfwSwapInterval(1);
     glEnable(GL_DEPTH_TEST);
 
     while (!glfwWindowShouldClose(window)) {
         glClearColor(0.0f,0.0f,0.0f,1.0f);
 
-        glm::mat4 projectionM = glm::ortho(-SCREEN_WIDTH/2.0f, SCREEN_WIDTH/2.0f, SCREEN_HEIGHT/2.0f, -SCREEN_HEIGHT/2.0f, -100000.0f, 1000.0f);//glm::perspective(glm::radians(45.0f),(float)SCREEN_WIDTH/(float)SCREEN_HEIGHT,0.1f,100.0f);
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glm::mat4 projectionM = glm::ortho(-display_w/2.0f, display_w/2.0f, display_h/2.0f, -display_h/2.0f, -100000.0f, 100000.0f);//glm::perspective(glm::radians(45.0f),(float)SCREEN_WIDTH/(float)SCREEN_HEIGHT,0.1f,100.0f);
         glm::mat4 viewM = glm::translate(glm::mat4(1.0f),glm::vec3(0.0f,0.0f,3.0f));
         viewM = glm::translate(viewM,glm::vec3(movex,movey,0.0f));
         glm::mat4 modelM = glm::mat4(1.0f);
         modelM = glm::scale(modelM,glm::vec3(scale));
         modelM = glm::rotate(modelM,glm::radians(180.0f),glm::vec3(0.0f,0.0f,1.0f));
         modelM = glm::rotate(modelM,glm::radians(30.0f),glm::vec3(1.0f,0.0f,0.0f));
-        modelM = glm::rotate(modelM,glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
+        modelM = glm::rotate(modelM,glm::radians(rotationx), glm::vec3(0.0f, 1.0f, 0.0f));
+        // modelM = glm::rotate(modelM,glm::radians(rotationy), glm::vec3(1.0f, 0.0f, 0.0f));
         modelM = glm::translate(modelM,glm::vec3(-(lasfile.header.x_min+lasfile.header.x_max)/2.0,-(lasfile.header.z_min+lasfile.header.z_max)/2.0,-(lasfile.header.y_min+lasfile.header.y_max)/2.0));
         glUniformMatrix4fv(glGetUniformLocation(shaderProgram,"model"),1,GL_FALSE,glm::value_ptr(modelM));
         glUniformMatrix4fv(glGetUniformLocation(shaderProgram,"view"),1,GL_FALSE,&viewM[0][0]);
         glUniformMatrix4fv(glGetUniformLocation(shaderProgram,"projection"),1,GL_FALSE,glm::value_ptr(projectionM));
-    
-        // glm::mat4 transM = glm::mat4(1.0f);
-        // transM = glm::scale(transM,glm::vec3(scale));
-        // // transM = glm::scale(transM,glm::vec3(0.0015));
-        // transM = glm::rotate(transM,glm::radians(30.0f),glm::vec3(-1.0,0.0,0.0));
-        // // transM = glm::rotate(transM,(float)glfwGetTime()/5, glm::vec3(0.0f, 1.0f, 0.0f));
-        // transM = glm::rotate(transM,glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
-        // transM = glm::translate(transM,glm::vec3(-(lasfile.header.x_min+lasfile.header.x_max)/2.0,-(lasfile.header.z_min+lasfile.header.z_max)/2.0,-(lasfile.header.y_min+lasfile.header.y_max)/2.0));
-        // transM = glm::translate(transM,glm::vec3(movex,movey,0.0));
-        // glUniformMatrix4fv(glGetUniformLocation(shaderProgram,"transform"),1,GL_FALSE,glm::value_ptr(transM));
 
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
         glDrawArrays(GL_POINTS, 0, lasfile.header.numOfPointRecords);
+        glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        {
+            ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNav;
+            ImGui::Begin("control",(bool*)true,window_flags);
+            ImGui::Text("view:");
+            if (ImGui::Button("reset")) {
+                rotationx = 0.0;
+                scale = 0.6;
+                movex = 0.0;
+                movey = 0.0;
+            }
+            ImGui::End();
+        }
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
         glFinish();
         glfwSwapBuffers(window);
         glFinish();
-        glfwPollEvents();
         glFlush();
     }
 
